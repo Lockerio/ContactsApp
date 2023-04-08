35520332ff26c2d0347be60332b10e319eb89980
Lockerio <goryunov.arkadiy@gmail.com>
2023-04-08 14:35:52 +0700

    Fix(ContactsApp.View): Rename labels in 'AboutForm'

--- src/ContactsApp/ContactsApp.View/AboutForm.Designer.cs
+++ src/ContactsApp/ContactsApp.View/AboutForm.Designer.cs
@@ -29,134 +29,137 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AboutForm));
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
+            this.TitleLabel = new System.Windows.Forms.Label();
+            this.VersionLabel = new System.Windows.Forms.Label();
+            this.AuthorLabel = new System.Windows.Forms.Label();
+            this.EmailLabel = new System.Windows.Forms.Label();
+            this.GithubLabel = new System.Windows.Forms.Label();
+            this.AuthorEmailLabel = new System.Windows.Forms.Label();
+            this.AuthorNameLabel = new System.Windows.Forms.Label();
             this.GithubLinkLabel = new System.Windows.Forms.LinkLabel();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.panel1 = new System.Windows.Forms.Panel();
+            this.LicenceTextBox = new System.Windows.Forms.RichTextBox();
+            this.OKPanel = new System.Windows.Forms.Panel();
             this.OKButton = new System.Windows.Forms.Button();
             this.IconsLinkLabel = new System.Windows.Forms.LinkLabel();
-            this.label6 = new System.Windows.Forms.Label();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.panel1.SuspendLayout();
-            this.panel2.SuspendLayout();
+            this.InfoLabel = new System.Windows.Forms.Label();
+            this.InfoPanel = new System.Windows.Forms.Panel();
+            this.OKPanel.SuspendLayout();
+            this.InfoPanel.SuspendLayout();
             this.SuspendLayout();
             // 
-            // label1
+            // TitleLabel
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(12, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(198, 36);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "ContactsApp";
+            this.TitleLabel.AutoSize = true;
+            this.TitleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.TitleLabel.Location = new System.Drawing.Point(9, 7);
+            this.TitleLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.TitleLabel.Name = "TitleLabel";
+            this.TitleLabel.Size = new System.Drawing.Size(198, 36);
+            this.TitleLabel.TabIndex = 0;
+            this.TitleLabel.Text = "ContactsApp";
             // 
-            // label2
+            // VersionLabel
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(15, 45);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(34, 16);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "v 1.0";
+            this.VersionLabel.AutoSize = true;
+            this.VersionLabel.Location = new System.Drawing.Point(11, 37);
+            this.VersionLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.VersionLabel.Name = "VersionLabel";
+            this.VersionLabel.Size = new System.Drawing.Size(31, 13);
+            this.VersionLabel.TabIndex = 1;
+            this.VersionLabel.Text = "v 1.0";
             // 
-            // label3
+            // AuthorLabel
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 85);
-            this.label3.Margin = new System.Windows.Forms.Padding(3, 0, 3, 12);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(48, 16);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "Author:";
+            this.AuthorLabel.AutoSize = true;
+            this.AuthorLabel.Location = new System.Drawing.Point(9, 69);
+            this.AuthorLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 10);
+            this.AuthorLabel.Name = "AuthorLabel";
+            this.AuthorLabel.Size = new System.Drawing.Size(41, 13);
+            this.AuthorLabel.TabIndex = 2;
+            this.AuthorLabel.Text = "Author:";
             // 
-            // label4
+            // EmailLabel
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(12, 113);
-            this.label4.Margin = new System.Windows.Forms.Padding(3, 0, 3, 12);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(48, 16);
-            this.label4.TabIndex = 3;
-            this.label4.Text = "E-mail:";
+            this.EmailLabel.AutoSize = true;
+            this.EmailLabel.Location = new System.Drawing.Point(9, 92);
+            this.EmailLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 10);
+            this.EmailLabel.Name = "EmailLabel";
+            this.EmailLabel.Size = new System.Drawing.Size(38, 13);
+            this.EmailLabel.TabIndex = 3;
+            this.EmailLabel.Text = "E-mail:";
             // 
-            // label5
+            // GithubLabel
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(12, 141);
-            this.label5.Margin = new System.Windows.Forms.Padding(3, 0, 3, 6);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(48, 16);
-            this.label5.TabIndex = 4;
-            this.label5.Text = "Github:";
+            this.GithubLabel.AutoSize = true;
+            this.GithubLabel.Location = new System.Drawing.Point(9, 115);
+            this.GithubLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 5);
+            this.GithubLabel.Name = "GithubLabel";
+            this.GithubLabel.Size = new System.Drawing.Size(41, 13);
+            this.GithubLabel.TabIndex = 4;
+            this.GithubLabel.Text = "Github:";
             // 
-            // label7
+            // AuthorEmailLabel
             // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(66, 113);
-            this.label7.Margin = new System.Windows.Forms.Padding(3, 0, 3, 12);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(187, 16);
-            this.label7.TabIndex = 6;
-            this.label7.Text = "goryunov.arkadiy@gmail.com";
+            this.AuthorEmailLabel.AutoSize = true;
+            this.AuthorEmailLabel.Location = new System.Drawing.Point(50, 92);
+            this.AuthorEmailLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 10);
+            this.AuthorEmailLabel.Name = "AuthorEmailLabel";
+            this.AuthorEmailLabel.Size = new System.Drawing.Size(146, 13);
+            this.AuthorEmailLabel.TabIndex = 6;
+            this.AuthorEmailLabel.Text = "goryunov.arkadiy@gmail.com";
             // 
-            // label8
+            // AuthorNameLabel
             // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(66, 85);
-            this.label8.Margin = new System.Windows.Forms.Padding(3, 0, 3, 12);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(114, 16);
-            this.label8.TabIndex = 5;
-            this.label8.Text = "Goryunov Arkadiy";
+            this.AuthorNameLabel.AutoSize = true;
+            this.AuthorNameLabel.Location = new System.Drawing.Point(50, 69);
+            this.AuthorNameLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 10);
+            this.AuthorNameLabel.Name = "AuthorNameLabel";
+            this.AuthorNameLabel.Size = new System.Drawing.Size(91, 13);
+            this.AuthorNameLabel.TabIndex = 5;
+            this.AuthorNameLabel.Text = "Goryunov Arkadiy";
             // 
             // GithubLinkLabel
             // 
             this.GithubLinkLabel.AutoSize = true;
-            this.GithubLinkLabel.Location = new System.Drawing.Point(66, 141);
+            this.GithubLinkLabel.Location = new System.Drawing.Point(50, 115);
+            this.GithubLinkLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.GithubLinkLabel.Name = "GithubLinkLabel";
-            this.GithubLinkLabel.Size = new System.Drawing.Size(167, 16);
+            this.GithubLinkLabel.Size = new System.Drawing.Size(141, 13);
             this.GithubLinkLabel.TabIndex = 7;
             this.GithubLinkLabel.TabStop = true;
             this.GithubLinkLabel.Text = "https://github.com/Lockerio";
             this.GithubLinkLabel.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.GithubLinkLabel_LinkClicked);
             // 
-            // richTextBox1
+            // LicenceTextBox
             // 
-            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.LicenceTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.richTextBox1.Location = new System.Drawing.Point(12, 166);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(640, 272);
-            this.richTextBox1.TabIndex = 8;
-            this.richTextBox1.Text = resources.GetString("richTextBox1.Text");
-            this.richTextBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.richTextBox1_KeyPress);
+            this.LicenceTextBox.Location = new System.Drawing.Point(9, 135);
+            this.LicenceTextBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.LicenceTextBox.Name = "LicenceTextBox";
+            this.LicenceTextBox.Size = new System.Drawing.Size(481, 222);
+            this.LicenceTextBox.TabIndex = 8;
+            this.LicenceTextBox.Text = resources.GetString("LicenceTextBox.Text");
+            this.LicenceTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.richTextBox1_KeyPress);
             // 
-            // panel1
+            // OKPanel
             // 
-            this.panel1.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.panel1.Controls.Add(this.OKButton);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel1.Location = new System.Drawing.Point(0, 484);
-            this.panel1.Margin = new System.Windows.Forms.Padding(0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(664, 60);
-            this.panel1.TabIndex = 25;
+            this.OKPanel.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.OKPanel.Controls.Add(this.OKButton);
+            this.OKPanel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.OKPanel.Location = new System.Drawing.Point(0, 393);
+            this.OKPanel.Margin = new System.Windows.Forms.Padding(0);
+            this.OKPanel.Name = "OKPanel";
+            this.OKPanel.Size = new System.Drawing.Size(498, 49);
+            this.OKPanel.TabIndex = 25;
             // 
             // OKButton
             // 
             this.OKButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.OKButton.Location = new System.Drawing.Point(551, 19);
-            this.OKButton.Margin = new System.Windows.Forms.Padding(4);
+            this.OKButton.Location = new System.Drawing.Point(413, 15);
             this.OKButton.Name = "OKButton";
-            this.OKButton.Size = new System.Drawing.Size(100, 28);
+            this.OKButton.Size = new System.Drawing.Size(75, 23);
             this.OKButton.TabIndex = 1;
             this.OKButton.Text = "OK";
             this.OKButton.UseVisualStyleBackColor = true;
@@ -168,61 +171,61 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.IconsLinkLabel.AutoSize = true;
-            this.IconsLinkLabel.Location = new System.Drawing.Point(251, 12);
-            this.IconsLinkLabel.Margin = new System.Windows.Forms.Padding(3, 12, 0, 0);
+            this.IconsLinkLabel.Location = new System.Drawing.Point(188, 10);
+            this.IconsLinkLabel.Margin = new System.Windows.Forms.Padding(2, 10, 0, 0);
             this.IconsLinkLabel.Name = "IconsLinkLabel";
-            this.IconsLinkLabel.Size = new System.Drawing.Size(60, 16);
+            this.IconsLinkLabel.Size = new System.Drawing.Size(50, 13);
             this.IconsLinkLabel.TabIndex = 27;
             this.IconsLinkLabel.TabStop = true;
             this.IconsLinkLabel.Text = "icons8.ru";
             this.IconsLinkLabel.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.IconsLinkLabel_LinkClicked);
             // 
-            // label6
+            // InfoLabel
             // 
-            this.label6.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.InfoLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(12, 12);
-            this.label6.Margin = new System.Windows.Forms.Padding(0, 12, 3, 0);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(233, 16);
-            this.label6.TabIndex = 26;
-            this.label6.Text = "All used images are downloaded from";
+            this.InfoLabel.AutoSize = true;
+            this.InfoLabel.Location = new System.Drawing.Point(9, 10);
+            this.InfoLabel.Margin = new System.Windows.Forms.Padding(0, 10, 2, 0);
+            this.InfoLabel.Name = "InfoLabel";
+            this.InfoLabel.Size = new System.Drawing.Size(182, 13);
+            this.InfoLabel.TabIndex = 26;
+            this.InfoLabel.Text = "All used images are downloaded from";
             // 
-            // panel2
+            // InfoPanel
             // 
-            this.panel2.Controls.Add(this.label6);
-            this.panel2.Controls.Add(this.IconsLinkLabel);
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel2.Location = new System.Drawing.Point(0, 444);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(664, 40);
-            this.panel2.TabIndex = 28;
+            this.InfoPanel.Controls.Add(this.InfoLabel);
+            this.InfoPanel.Controls.Add(this.IconsLinkLabel);
+            this.InfoPanel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.InfoPanel.Location = new System.Drawing.Point(0, 361);
+            this.InfoPanel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.InfoPanel.Name = "InfoPanel";
+            this.InfoPanel.Size = new System.Drawing.Size(498, 32);
+            this.InfoPanel.TabIndex = 28;
             // 
             // AboutForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(664, 544);
-            this.Controls.Add(this.panel2);
-            this.Controls.Add(this.panel1);
-            this.Controls.Add(this.richTextBox1);
+            this.ClientSize = new System.Drawing.Size(498, 442);
+            this.Controls.Add(this.InfoPanel);
+            this.Controls.Add(this.OKPanel);
+            this.Controls.Add(this.LicenceTextBox);
             this.Controls.Add(this.GithubLinkLabel);
-            this.Controls.Add(this.label7);
-            this.Controls.Add(this.label8);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Controls.Add(this.AuthorEmailLabel);
+            this.Controls.Add(this.AuthorNameLabel);
+            this.Controls.Add(this.GithubLabel);
+            this.Controls.Add(this.EmailLabel);
+            this.Controls.Add(this.AuthorLabel);
+            this.Controls.Add(this.VersionLabel);
+            this.Controls.Add(this.TitleLabel);
             this.Name = "AboutForm";
             this.ShowIcon = false;
-            this.panel1.ResumeLayout(false);
-            this.panel2.ResumeLayout(false);
-            this.panel2.PerformLayout();
+            this.OKPanel.ResumeLayout(false);
+            this.InfoPanel.ResumeLayout(false);
+            this.InfoPanel.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -230,19 +233,19 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label TitleLabel;
+        private System.Windows.Forms.Label VersionLabel;
+        private System.Windows.Forms.Label AuthorLabel;
+        private System.Windows.Forms.Label EmailLabel;
+        private System.Windows.Forms.Label GithubLabel;
+        private System.Windows.Forms.Label AuthorEmailLabel;
+        private System.Windows.Forms.Label AuthorNameLabel;
         private System.Windows.Forms.LinkLabel GithubLinkLabel;
-        private System.Windows.Forms.RichTextBox richTextBox1;
-        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RichTextBox LicenceTextBox;
+        private System.Windows.Forms.Panel OKPanel;
         private System.Windows.Forms.Button OKButton;
         private System.Windows.Forms.LinkLabel IconsLinkLabel;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label InfoLabel;
+        private System.Windows.Forms.Panel InfoPanel;
     }
 }
